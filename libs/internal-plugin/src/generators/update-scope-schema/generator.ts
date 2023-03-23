6ae5d4a0cca3f2bfcc132bf71387f198bbbad889
HAVEN\fspadm <mail@florian-spier.be>
2023-03-23 11:57:43 +0100

    Generated generator

--- libs/internal-plugin/src/generators/update-scope-schema/generator.ts
+++ libs/internal-plugin/src/generators/update-scope-schema/generator.ts
@@ -1,66 +1,69 @@
 import {
-  Tree,
-  updateJson,
+  addProjectConfiguration,
   formatFiles,
-  ProjectConfiguration,
-  getProjects,
-  updateProjectConfiguration,
+  generateFiles,
+  getWorkspaceLayout,
+  names,
+  offsetFromRoot,
+  Tree,
 } from '@nrwl/devkit';
+import * as path from 'path';
+import { UpdateScopeSchemaGeneratorSchema } from './schema';
 
-export default async function (tree: Tree) {
-  addScopeIfMissing(tree);
-  const scopes = getScopes(getProjects(tree));
-  updateSchemaJson(tree, scopes);
-  updateSchemaInterface(tree, scopes);
-  await formatFiles(tree);
+interface NormalizedSchema extends UpdateScopeSchemaGeneratorSchema {
+  projectName: string;
+  projectRoot: string;
+  projectDirectory: string;
+  parsedTags: string[];
 }
 
-function addScopeIfMissing(tree: Tree) {
-  const projectMap = getProjects(tree);
-  Array.from(projectMap.keys()).forEach((projectName) => {
-    const project = projectMap.get(projectName);
-    if (!project.tags.some((tag) => tag.startsWith('scope:'))) {
-      const scope = projectName.split('-')[0];
-      project.tags.push(`scope:${scope}`);
-      updateProjectConfiguration(tree, projectName, project);
-    }
-  });
+function normalizeOptions(tree: Tree, options: UpdateScopeSchemaGeneratorSchema): NormalizedSchema {
+  const name = names(options.name).fileName;
+  const projectDirectory = options.directory
+    ? `${names(options.directory).fileName}/${name}`
+    : name;
+  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
+  const projectRoot = `${getWorkspaceLayout(tree).libsDir}/${projectDirectory}`;
+  const parsedTags = options.tags
+    ? options.tags.split(',').map((s) => s.trim())
+    : [];
+
+  return {
+    ...options,
+    projectName,
+    projectRoot,
+    projectDirectory,
+    parsedTags,
+  };
 }
 
-function getScopes(projectMap: Map<string, ProjectConfiguration>) {
-  const projects: any[] = Array.from(projectMap.values());
-  const allScopes: string[] = projects
-    .map((project) =>
-      project.tags.filter((tag: string) => tag.startsWith('scope:'))
-    )
-    .reduce((acc, tags) => [...acc, ...tags], [])
-    .map((scope: string) => scope.slice(6));
-  return Array.from(new Set(allScopes));
+function addFiles(tree: Tree, options: NormalizedSchema) {
+    const templateOptions = {
+      ...options,
+      ...names(options.name),
+      offsetFromRoot: offsetFromRoot(options.projectRoot),
+      template: ''
+    };
+    generateFiles(tree, path.join(__dirname, 'files'), options.projectRoot, templateOptions);
 }
 
-function updateSchemaJson(tree: Tree, scopes: string[]) {
-  updateJson(
+export default async function (tree: Tree, options: UpdateScopeSchemaGeneratorSchema) {
+  const normalizedOptions = normalizeOptions(tree, options);
+  addProjectConfiguration(
     tree,
-    'libs/internal-plugin/src/generators/util-lib/schema.json',
-    (schemaJson) => {
-      schemaJson.properties.directory['x-prompt'].items = scopes.map(
-        (scope) => ({
-          value: scope,
-          label: scope,
-        })
-      );
-      return schemaJson;
+    normalizedOptions.projectName,
+    {
+      root: normalizedOptions.projectRoot,
+      projectType: 'library',
+      sourceRoot: `${normalizedOptions.projectRoot}/src`,
+      targets: {
+        build: {
+          executor: "@bg-hoard/internal-plugin:build",
+        },
+      },
+      tags: normalizedOptions.parsedTags,
     }
   );
-}
-
-function updateSchemaInterface(tree: Tree, scopes: string[]) {
-  const joinScopes = scopes.map((s) => `'${s}'`).join(' | ');
-  const interfaceDefinitionFilePath =
-    'libs/internal-plugin/src/generators/util-lib/schema.d.ts';
-  const newContent = `export interface UtilLibGeneratorSchema {
-  name: string;
-  directory: ${joinScopes};
-}`;
-  tree.write(interfaceDefinitionFilePath, newContent);
+  addFiles(tree, normalizedOptions);
+  await formatFiles(tree);
 }
